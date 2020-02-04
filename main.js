7df54d81e704fa7fdba05ec6dfe403a0362ea357
India Rodriguez <carnes.s.ia17@gmail.com>
2020-02-03 17:16:28 -0700

    img modals now working.

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
 // Get the modal
 var modal = document.getElementById("myModal");
-
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
+// var img = document.getElementById("myImg");
+
+var images = document.getElementsByClassName("myImg");
+
 var modalImg = document.getElementById("img01");
+
 var captionText = document.getElementById("caption");
-img.onclick = function () {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
+
+for (var i = 0; i < images.length; i++) {
+    var img = images[i];
+
+    img.onclick = function (evt) {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
 }
 
 // Get the <span> element that closes the modal
